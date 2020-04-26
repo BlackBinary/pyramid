0480aa8b84bd8a6327613b39cdd97a7d5eec2cd4
Daen Rebel <daen@index.nl>
2020-04-27 00:15:59 +0200

    Added interval because i can

--- src/core/bot/index.js
+++ src/core/bot/index.js
@@ -1,6 +1,13 @@
 const logger = require('@lib/logger');
 
+const bot = () => {
+  logger.info('SSSSH! Bot is trading');
+};
+
 module.exports = () => {
   logger.info('Let me tell you something. There’s no nobility in poverty. I’ve been a rich man and I’ve been a poor man. And I choose rich every f**king time. – Jordan Belfort');
   logger.info('Starting CPTB! Feel free to abort while you can');
+  const botInterval = setInterval(() => {
+    bot();
+  }, 1000);
 };
