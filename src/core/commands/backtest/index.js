5a7a9779d11a808a050321cfd8b68f1ae4cfe869
Daen Rebel <daenrebel035@gmail.com>
2020-05-02 23:46:39 +0200

    Backtesting successful

--- src/core/commands/backtest/index.js
+++ src/core/commands/backtest/index.js
@@ -26,11 +26,37 @@ module.exports.getMarketData = async () => {
 };
 
 module.exports.data = {
-  high: [],
-  low: [],
-  open: [],
+  // high: [],
+  // low: [],
+  // open: [],
   close: [],
-  volume: [],
+  // volume: [],
+};
+
+module.exports.portfolio = {
+  usd: 0,
+  btc: 0,
+};
+
+module.exports.SELL = 'SELL';
+module.exports.BUY = 'BUY';
+
+module.exports.trades = [];
+
+module.exports.trade = (amount, price, type) => {
+  if (type === this.BUY) {
+    logger.info(`[BACKTESTING] Buying ${amount} at ${price}`);
+    this.portfolio.usd -= amount * price;
+    this.portfolio.btc = amount;
+    this.trades.push({ type: this.BUY, price, amount });
+  } else if (type === this.SELL) {
+    logger.info(`[BACKTESTING] Selling ${amount} at ${price}`);
+    this.portfolio.usd += amount * price;
+    this.portfolio.btc -= amount;
+    this.trades.push({ type: this.SELL, price, amount });
+  } else {
+    logger.info('[BACKTESTING] Invalid trade type');
+  }
 };
 
 module.exports = async () => {
@@ -56,6 +82,10 @@ module.exports = async () => {
   // Average over set periods
   const averageOver = 5;
 
+  // Set an initial portfolio to start trading with
+  this.portfolio.usd = 100;
+  const startedWith = { ...this.portfolio };
+
   // First try with SMA over 5 periods
   tulind.indicators.sma.indicator([this.data.close], [averageOver], (err, results) => {
     // Get the results
@@ -76,18 +106,41 @@ module.exports = async () => {
 
       // If the difference is a positive number, do something (buy?)
       if (smaDifference > 0) {
-        logger.info(`Closing SMA is up ${smaDifference}`);
+        logger.info(`[BACKTESTING] Closing SMA is up ${smaDifference}`);
+        if (this.portfolio.usd > 0) {
+          const amount = this.portfolio.usd / currentPrice;
+          this.trade(amount, currentPrice, this.BUY);
+        }
       } else if (smaDifference < 0) { // If the difference is a negative number, do something (sell?)
-        logger.info(`Closing SMA is down ${smaDifference}`);
+        logger.info(`[BACKTESTING] Closing SMA is down ${smaDifference}`);
+        if (this.portfolio.btc > 0) {
+          this.trade(this.portfolio.btc, currentPrice, this.SELL);
+        }
       } else {
-        logger.info('Closing SMA is equal');
+        logger.info('[BACKTESTING] Closing SMA is equal');
       }
 
-      logger.info(`Price Previous: ${previousPrice}`);
-      logger.info(`Price Current:  ${currentPrice}`);
+      logger.info(`[BACKTESTING] Price Previous: ${previousPrice}`);
+      logger.info(`[BACKTESTING] Price Current:  ${currentPrice}`);
 
-      logger.info(`SMA Previous: ${previousSma}`);
-      logger.info(`SMA Current:  ${currentSma}`);
+      logger.info(`[BACKTESTING] SMA Previous: ${previousSma}`);
+      logger.info(`[BACKTESTING] SMA Current:  ${currentSma}`);
     }
+
+    logger.info('\n\n\n\n');
+    logger.info('[BACKTESTING] Results');
+    logger.info(`[BACKTESTING] Total trades done: ${this.trades.length}`);
+
+    logger.info('[BACKTESTING] Started With:');
+    logger.info(`[BACKTESTING] USD: ${startedWith.usd}`);
+    logger.info(`[BACKTESTING] BTC: ${startedWith.btc}`);
+
+    logger.info('[BACKTESTING] Ended With:');
+    logger.info(`[BACKTESTING] USD: ${this.portfolio.usd}`);
+    logger.info(`[BACKTESTING] BTC: ${this.portfolio.btc}`);
+
+    const totalProfits = this.portfolio.usd + (this.portfolio.btc * this.data.close[this.data.close.length - 1]);
+    logger.info('[BACKTESTING] Potential profits:');
+    logger.info(`[BACKTESTING] USD: ${totalProfits}`);
   });
 };
