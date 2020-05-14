85c255b803aa660d8a2950775f465f0423b1a165
Tycho Kaspers <tycho@index.nl>
2020-05-15 01:31:27 +0200

    update config

--- vue.config.js
+++ vue.config.js
@@ -19,4 +19,11 @@ module.exports = {
       },
     },
   },
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: '@import "@assets/styles/_variables.scss";',
+      },
+    },
+  },
 };
