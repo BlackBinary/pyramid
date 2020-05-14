9a2349a2724b5d86e944f3e6c9cc478de454d0cc
Daen Rebel <daen@index.nl>
2020-05-15 00:31:22 +0200

    Clean setup

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+const path = require('path');
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.vue'],
+      },
+      alias: {
+        map: [
+          ['@root', './'],
+          ['@frontend', path.resolve(__dirname, 'frontend/src/')],
+          ['@backend', path.resolve(__dirname, 'backend/src/')],
+          ['@lib', path.resolve(__dirname, 'backend/src/lib')],
+          ['@core', path.resolve(__dirname, 'backend/src/core')],
+          ['@strategies', path.resolve(__dirname, 'strategies')],
+        ],
+      },
+    },
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        mocha: true,
+      },
+    },
+  ],
+};
