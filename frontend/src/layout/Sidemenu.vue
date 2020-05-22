05b6480a407137c78b45e7f82932cb8673bc076e
Daen Rebel <daenrebel035@gmail.com>
2020-05-22 21:47:46 +0200

    better look v2

--- frontend/src/layout/Sidemenu.vue
+++ frontend/src/layout/Sidemenu.vue
@@ -1,10 +1,11 @@
 <template lang="pug">
   .row.layout-basic
-    .col-xs-3.sidemenu
+    .col-xs-2.sidemenu
       router-link.nav-item(v-for="item in menuItems" :to="item.to" :key="item.title")
         i.fa.fal(:class="item.class")
         p {{ item.title }}
-    .col-xs-9
+      img.sidemenu-background(alt="Pyramid Project Gradient" src="@assets/login-background.png")
+    .col-xs-10
       router-view
 </template>
 
@@ -51,24 +52,30 @@ export default {
   .layout-basic {
     min-height: 100vh;
     .sidemenu {
-      background: $blackCoral;
+      background: $gunMetal;
       padding-top: 10px;
       .nav-item {
         text-decoration: none;
         display: flex;
         margin: 5px;
         padding: 16px;
-        background: $gunMetal;
+        background: $blackCoral;
         border-radius: 12px;
         font-family: $baseFont;
         color: $white;
         display: flex;
         &:hover {
-          background: lighten($gunMetal, 50%);
+          background: lighten($blackCoral, 20%);
         }
         i {
           margin-right: 10px;
         }
+      }
+      .sidemenu-background {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
       }
     }
   }
