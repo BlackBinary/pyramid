57f4a950b4279a02ea4adfd75793695f1ce4dd5c
Tycho Kaspers <tycho@index.nl>
2020-05-24 15:08:55 +0200

    base of datatable

---
+++ frontend/src/components/datatable/PyramidDataTable.vue
@@ -0,0 +1,74 @@
+<template lang="pug">
+.pyramid-datatable
+  table
+    thead
+      tr
+        th(v-for="header in headers") {{ header.name }}
+    tbody
+      tr(v-for="row in rows")
+        slot(v-bind:row="row")
+</template>
+
+<script>
+export default {
+  name: 'PyramidDataTable',
+  props: {
+    headers: {
+      type: Array,
+      default: () => [],
+    },
+    rows: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.pyramid-datatable {
+  background: $gunMetal;
+  border-radius: 5px;
+  padding: 15px;
+
+  table {
+    width: 100%;
+    text-align: left;
+
+    th {
+      color: $blackCoral;
+      text-transform: uppercase;
+      letter-spacing: 4px;
+      font-size: 10px;
+    }
+
+    th, td {
+      padding: 10px;
+      font-weight: 600;
+
+      &:first-child {
+        padding-left: 30px;
+      }
+
+      &:last-child {
+        padding-right: 30px;
+      }
+    }
+
+    tbody {
+      tr {
+        &:hover {
+          background: rgba(lighten($blackCoral, 0.1%), 0.4);
+        }
+        &:nth-child(even) {
+          background: rgba(lighten($blackCoral, 0.1%), 0.2);
+
+          &:hover {
+            background: rgba(lighten($blackCoral, 0.1%), 0.4);
+          }
+        }
+      }
+    }
+  }
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -5,7 +5,21 @@ div
   .row.m-b-xl
     .col-xs-9
       PyramidTabs
-        PyramidTab(name="Trade History" :selected="true") lalalalala
+        PyramidTab(name="Trade History" :selected="true")
+          PyramidDataTable(:headers="headers" :rows="rows")
+            template(v-slot="{ row }")
+              td(v-for="(cols, index) in row")
+                span(
+                  v-if="index === 0"
+                  :class='completeOrErrorClass(cols.type)'
+                )
+                  | {{ cols.type === 1 ? 'Sell' : 'Buy' }}
+                span(
+                  v-if="index === 5"
+                  :class='completeOrErrorClass(cols.status)'
+                )
+                  | {{ cols.status === 1 ? 'Denied' : 'Completed' }}
+                template(v-else) {{ cols.content }}
         PyramidTab(name="Order Book") lalalalalal 2
         PyramidTab(name="Active Orders") lalalalalal 2
     .col-xs-3
@@ -28,6 +42,108 @@ export default {
     return {
       myStrategies: [],
       addStrategyModal: false,
+      headers: [
+        {
+          name: 'Type',
+        },
+        {
+          name: 'Id',
+        },
+        {
+          name: 'Price',
+        },
+        {
+          name: 'Amount',
+        },
+        {
+          name: 'Total',
+        },
+        {
+          name: 'Status',
+        },
+      ],
+      rows: [
+        [
+          {
+            type: 0,
+          },
+          {
+            content: '30',
+          },
+          {
+            content: '9.005,90 EUR',
+          },
+          {
+            content: '0 BTC',
+          },
+          {
+            content: '10 BTC',
+          },
+          {
+            status: 0,
+          },
+        ],
+        [
+          {
+            type: 0,
+          },
+          {
+            content: '10',
+          },
+          {
+            content: '8.200,90 EUR',
+          },
+          {
+            content: '0 BTC',
+          },
+          {
+            content: '10 BTC',
+          },
+          {
+            status: 0,
+          },
+        ],
+        [
+          {
+            type: 0,
+          },
+          {
+            content: '10',
+          },
+          {
+            content: '8.200,90 EUR',
+          },
+          {
+            content: '0 BTC',
+          },
+          {
+            content: '10 BTC',
+          },
+          {
+            status: 1,
+          },
+        ],
+        [
+          {
+            type: 1,
+          },
+          {
+            content: '10',
+          },
+          {
+            content: '8.200,90 EUR',
+          },
+          {
+            content: '0 BTC',
+          },
+          {
+            content: '10 BTC',
+          },
+          {
+            status: 1,
+          },
+        ],
+      ],
     };
   },
   apollo: {
@@ -39,11 +155,15 @@ export default {
     addStrategy() {
       this.addStrategyModal = true;
     },
+    completeOrErrorClass(int) {
+      return int === 0 ? 'has-text-caribbean-green' : 'has-text-cerise';
+    },
   },
   components: {
     PyramidStrategyCard: () => import('@frontend/components/cards/PyramidStrategyCard'),
     PyramidTabs: () => import('@frontend/components/tabs/PyramidTabs'),
     PyramidTab: () => import('@frontend/components/tabs/PyramidTab'),
+    PyramidDataTable: () => import('@frontend/components/datatable/PyramidDataTable'),
     StrategyModal: () => import('@frontend/views/Strategies/modals/StrategyModal'),
   },
 };
